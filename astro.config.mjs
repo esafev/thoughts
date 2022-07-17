ff86797a23ac390a9df787ab6f1020a3e24cd869
Vlad Esafev <vlad@esafev.com>
2022-07-17 09:22:45 +0400

    Add robots.txt plugin (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+
 import preact from '@astrojs/preact';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-  integrations: [preact()]
+  site: 'https://esafev.com',
+  experimental: {
+    integrations: true,
+  },
+  integrations: [preact(), robotsTxt()]
 });

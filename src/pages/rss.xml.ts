61ddeb812bc3251c251a835444e75f22c78718f0
Vlad Esafev <vlad@esafev.com>
2022-07-19 13:02:31 +0400

    fit rss mapping

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,7 +6,11 @@ const get = () => rss({
   title: 'Vlad Esafev',
   description: 'Not causing trouble, not touching anything, fixing the primus',
   site: import.meta.env.SITE,
-  items: getBlogPosts(),
+  items: getBlogPosts().map((post) => ({
+    link: post.frontmatter.slug,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.pubDate,
+  }))
 });
 
 export { get };

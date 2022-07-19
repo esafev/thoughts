c27353b0e865b79dfc927876e9cb9ce417c7ee0d
Vlad Esafev <vlad@esafev.com>
2022-07-19 13:37:02 +0400

    entry point for blog posts (#13)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,17 @@
 import rss from '@astrojs/rss';
 
+import { getBlogPosts } from '../utils/posts';
+
 const get = () => rss({
   title: 'Vlad Esafev',
   description: 'Not causing trouble, not touching anything, fixing the primus',
   site: import.meta.env.SITE,
-  items: import.meta.glob('./posts/*.md'),
+  items: getBlogPosts().map((post) => ({
+    link: post.frontmatter.slug,
+    title: post.frontmatter.title,
+    description: post.frontmatter.description,
+    pubDate: post.frontmatter.pubDate
+  }))
 });
 
 export { get };
---
+++ src/utils/posts.ts
@@ -0,0 +1,7 @@
+// TODO: Type it
+const getBlogPosts = () => 
+  Object.values(import.meta.globEager('../pages/posts/*.md'))
+    .sort((a, b) => new Date(b.frontmatter.pubDate).valueOf() - new Date(a.frontmatter.pubDate).valueOf())
+    .filter(a => !a.frontmatter.publshed);
+
+export { getBlogPosts };

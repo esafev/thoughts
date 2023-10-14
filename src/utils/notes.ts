74170f991f203cfd9198ec08e63cd5bd6916e49f
Vlad Esafev <vlad@esafev.com>
2023-10-14 14:57:13 +0400

    .md -> .mdx

--- src/utils/notes.ts
+++ src/utils/notes.ts
@@ -1,6 +1,6 @@
 // TODO: Type it
 function getNotes () {
-  return Object.values(import.meta.glob('../pages/notes/*.md', { eager: true }))
+  return Object.values(import.meta.glob('../pages/notes/*.mdx', { eager: true }))
   .sort((a, b) => new Date(b.frontmatter.pubDate).valueOf() - new Date(a.frontmatter.pubDate).valueOf())
   .filter(a => !a.frontmatter.publshed);
 };

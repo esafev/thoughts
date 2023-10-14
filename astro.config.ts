830ad0a17b9276d8fb57e88fb1bd9207e0f41f9a
Vlad Esafev <artmare@me.com>
2023-10-14 14:53:43 +0400

    .md -> .mdx (#125)

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
+import mdx from '@astrojs/mdx';
 
 const config = {
   site: 'https://esafev.com',
   integrations: [
     preact(), 
     sitemap(), 
-    robotsTxt()
+    robotsTxt(),
+    mdx()
   ]
 };
 

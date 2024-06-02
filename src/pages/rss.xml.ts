8d1f7575cf45f572cdda329077550b1c3841492b
vlad <artmare@me.com>
2024-06-02 10:52:01 +0200

    Fix RSS Part 2 (#165)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,17 +1,18 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
 
-import { getNotes } from '@utils/notes';
+import { getNotes } from "@utils/notes";
 
-const get = () => rss({
-  title: 'Vlad Esafev',
-  description: 'Not causing trouble, not touching anything, fixing the primus',
-  site: import.meta.env.SITE,
-  items: getNotes().map((note) => ({
-    link: `notes/${note.frontmatter.slug}`,
-    title: note.frontmatter.title,
-    description: note.frontmatter.description,
-    pubDate: note.frontmatter.pubDate
-  }))
-});
-
-export { get };
+export function GET(context) {
+  return rss({
+    title: "Vlad Esafev",
+    description:
+      "Not causing trouble, not touching anything, fixing the primus",
+    site: context.site,
+    items: getNotes().map((note) => ({
+      link: `notes/${note.frontmatter.slug}`,
+      title: note.frontmatter.title,
+      description: note.frontmatter.description,
+      pubDate: note.frontmatter.pubDate,
+    })),
+  });
+}

a682efd9aa8d908ff064b569063ffd6aaa657517
vlad <artmare@me.com>
2024-06-02 09:46:56 +0200

    Added `vercel` adapter (#163)

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { AstroUserConfig, defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
-import mdx from '@astrojs/mdx';
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/static";
 
-const config = {
-  site: 'https://esafev.com',
-  integrations: [
-    preact(), 
-    sitemap(), 
-    robotsTxt(),
-    mdx()
-  ]
+const config: AstroUserConfig = {
+  site: "https://esafev.com",
+  output: "static",
+  integrations: [preact(), sitemap(), robotsTxt(), mdx()],
+  adapter: vercel({
+    imageService: true,
+  }),
 };
 
 export default defineConfig(config);

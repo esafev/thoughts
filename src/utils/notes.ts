0139c65cfb66d028befb5532ceb7f267bb0ccace
Vlad Esafev <vlad@esafev.com>
2024-06-23 10:29:06 +0200

    fix: `published` toggle

--- src/utils/notes.ts
+++ src/utils/notes.ts
@@ -1,8 +1,14 @@
 // TODO: Type it
-function getNotes () {
-  return Object.values(import.meta.glob('../pages/notes/*.mdx', { eager: true }))
-  .sort((a, b) => new Date(b.frontmatter.pubDate).valueOf() - new Date(a.frontmatter.pubDate).valueOf())
-  .filter(a => !a.frontmatter.publshed);
-};
+function getNotes() {
+  return Object.values(
+    import.meta.glob("../pages/notes/*.mdx", { eager: true }),
+  )
+    .sort(
+      (a, b) =>
+        new Date(b.frontmatter.pubDate).valueOf() -
+        new Date(a.frontmatter.pubDate).valueOf(),
+    )
+    .filter((a) => a.frontmatter.published);
+}
 
 export { getNotes };

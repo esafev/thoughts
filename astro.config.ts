7540d40ee625ab80f3a62eac10e4b005c2a4b750
Vlad Esafev <vlad@esafev.com>
2024-12-07 17:33:44 +0100

    remove cf adapter

--- astro.config.ts
+++ astro.config.ts
@@ -4,15 +4,11 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
-import cloudflare from '@astrojs/cloudflare';
 
 const config: AstroUserConfig = {
   site: "https://esafev.com",
   output: "static",
-  integrations: [preact(), sitemap(), robotsTxt(), mdx()],
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
+  integrations: [preact(), sitemap(), robotsTxt(), mdx()]
 };
 
 export default defineConfig(config);

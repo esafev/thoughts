e6423672819fa4f281a6be107b4ca21cae28a1b2
vlad <artmare@me.com>
2024-12-07 18:00:17 +0100

    Cloudflare Pages (#226)

--- astro.config.ts
+++ astro.config.ts
@@ -4,15 +4,11 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/static";
 
 const config: AstroUserConfig = {
   site: "https://esafev.com",
   output: "static",
-  integrations: [preact(), sitemap(), robotsTxt(), mdx()],
-  adapter: vercel({
-    imageService: true,
-  }),
+  integrations: [preact(), sitemap(), robotsTxt(), mdx()]
 };
 
 export default defineConfig(config);

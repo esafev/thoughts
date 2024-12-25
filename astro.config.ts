7305bda40d5922e67e5fb0829b4a5f101e907852
Vlad Esafev <vlad@esafev.com>
2024-12-25 11:36:47 +0100

    Remove `preact`

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { AstroUserConfig, defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
@@ -8,7 +7,7 @@ import mdx from "@astrojs/mdx";
 const config: AstroUserConfig = {
   site: "https://esafev.com",
   output: "static",
-  integrations: [preact(), sitemap(), robotsTxt(), mdx()]
+  integrations: [sitemap(), robotsTxt(), mdx()]
 };
 
 export default defineConfig(config);

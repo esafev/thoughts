8510ecf925181b8b7562b96b90882aa23b52c493
Vlad Esafev <vlad@esafev.com>
2024-12-25 16:32:09 +0100

    Dynamic robots txt

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,12 +2,11 @@ import rss from "@astrojs/rss";
 
 import { getNotes } from "@utils/notes";
 
-export function GET(context) {
+export function GET({ site }) {
   return rss({
     title: "Vlad Esafev",
-    description:
-      "Not causing trouble, not touching anything, fixing the primus",
-    site: context.site,
+    description: "Not causing trouble, not touching anything, fixing the primus",
+    site,
     items: getNotes().map((note) => ({
       link: `notes/${note.frontmatter.slug}`,
       title: note.frontmatter.title,

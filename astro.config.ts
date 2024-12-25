eddd93bf3990f364a9c58cb4fdd4db74cc119548
Vlad Esafev <vlad@esafev.com>
2024-12-25 13:28:02 +0100

    remove lib

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,12 @@
 import { AstroUserConfig, defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
 
 const config: AstroUserConfig = {
   site: "https://esafev.com",
   output: "static",
-  integrations: [sitemap(), robotsTxt(), mdx()]
+  integrations: [sitemap(), mdx()]
 };
 
 export default defineConfig(config);

8db71abe3650aabe3fcf53898188e509a993f795
Vlad Esafev <vlad@esafev.com>
2025-07-12 20:46:57 +0200

    RSS: Return HTML content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,11 @@
 import rss from "@astrojs/rss";
+import { experimental_AstroContainer } from "astro/container";
+import mdxRenderer from "@astrojs/mdx/server.js";
 
 import { getNotes } from "@/utils/notes";
+
+const container = await experimental_AstroContainer.create();
+container.addServerRenderer({ renderer: mdxRenderer });
 
 export async function GET({ site }) {
   const notes = await getNotes();
@@ -8,11 +13,19 @@ export async function GET({ site }) {
     title: "Vlad Esafev",
     description: "Not causing trouble, not touching anything, fixing the primus",
     site,
-    items: notes.map((note) => ({
-      link: `notes/${note.slug}`,
-      title: note.data.title,
-      description: note.data.description,
-      pubDate: note.data.pubDate,
-    })),
+    items: await Promise.all(
+      notes.map(async (note) => {
+        const { Content } = await note.render();
+        const htmlContent = await container.renderToString(Content);
+
+        return {
+          link: `notes/${note.slug}`,
+          title: note.data.title,
+          description: note.data.description,
+          pubDate: note.data.pubDate,
+          content: htmlContent
+        };
+      })
+    ),
   });
 }
